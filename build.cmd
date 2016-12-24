d1fab0af3ec320e348bfc4d179d5a6712dd2f9c8
Chris Gomez <cgomez@medicity.com>
2016-12-24 13:38:35 -0500

    automated build and minification of library

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 mkdir src
 mkdir obj
-copy lib\firstClass_ramda.js obj /y
-del obj\browserSafe_folktale.js
-browserify ./lib/browserify_folktale.js -o ./obj/browserSafe_folktale.js
+copy lib\firstclass_ramda.js obj /y
+del obj\browserified_folktale.js
+browserify ./lib/require_folktale.js -o ./obj/browserified_folktale.js
--- samples/task/lib/firstClass_ramda.js
+++
@@ -1,4 +0,0 @@
-this.curry = R.curry;
-this.compose = R.compose;
-this.map = R.map;
-this.chain = R.chain;
